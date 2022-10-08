c27612071a8fdbbe5621ec2d2029d714549cc9e2
Carson Hopper <carson.hopper@outlook.com>
2022-10-08 13:08:16 -0700

    Remove blade views

--- resources/views/stern/pages/servers/dedicated.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('stern.layouts.main', ['title' => 'Dedicated Servers'])
-
-@section('content')
-    <div class="w-full px-6 mx-auto">
-
-    </div>
-
-    <x-Stern.Footer />
-@endsection
--- resources/views/stern/pages/servers/vm.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('stern.layouts.main', ['title' => 'Virtual Machine'])
-
-@section('content')
-    <div class="w-full px-6 mx-auto">
-
-    </div>
-
-    <x-Stern.Footer />
-@endsection
