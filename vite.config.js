3fac4a6a7344f9727cae5c9b0084840d71a8a2b6
Carson Hopper <carson.hopper@outlook.com>
2022-10-08 13:24:02 -0700

    Allow full path resolves using '@'

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
     server: {
@@ -27,6 +28,11 @@ export default defineConfig({
             ]
         })
     ],
+    resolve: {
+        alias: {
+            '@/': `${path.resolve(__dirname, 'resources/vue')}/`
+        },
+    },
     build: {
         watch: {
         // https://rollupjs.org/guide/en/#watch-options
