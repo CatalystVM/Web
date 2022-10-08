a7435c38a8176b13cd3c9c302c983d1cf77e94cb
Carson Hopper <carson.hopper@outlook.com>
2022-10-08 13:24:54 -0700

    Allow checking permissions on each routes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider {
@@ -13,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider {
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        \App\Models\User::class => \App\Policies\UserPolicy::class,
     ];
 
     /**
@@ -24,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider {
     public function boot() {
         $this->registerPolicies();
 
-        //
+        Gate::define('view-stern', function() {
+            return Auth::check() ? Auth::user()->HasPermission('stern.view') : false;
+        });
     }
 }
