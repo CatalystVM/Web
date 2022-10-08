baa497e4ea8c55719d79f63313a190f7a5024223
Carson Hopper <carson.hopper@outlook.com>
2022-10-08 13:10:18 -0700

    Remove unused fonts

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,18 +18,18 @@ import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fort
 /* import specific icons */
 import { fas } from '@fortawesome/pro-solid-svg-icons'
 import { far } from '@fortawesome/pro-regular-svg-icons'
-import { fal } from '@fortawesome/pro-light-svg-icons'
-import { fat } from '@fortawesome/pro-thin-svg-icons'
-import { fad } from '@fortawesome/pro-duotone-svg-icons'
-import { fass } from '@fortawesome/sharp-solid-svg-icons'
+//import { fal } from '@fortawesome/pro-light-svg-icons'
+//import { fat } from '@fortawesome/pro-thin-svg-icons'
+//import { fad } from '@fortawesome/pro-duotone-svg-icons'
+//import { fass } from '@fortawesome/sharp-solid-svg-icons'
 
 /* add icons to the library */
 library.add(fas)
 library.add(far)
-library.add(fal)
-library.add(fat)
-library.add(fad)
-library.add(fass)
+//library.add(fal)
+//library.add(fat)
+//library.add(fad)
+//library.add(fass)
 
 function resolvePageComponent(name, pages) {
     for (const path in pages) {
