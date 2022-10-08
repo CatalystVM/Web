11b876e68e9c661ee1e3e626b07e881d729f145e
Carson Hopper <carson.hopper@outlook.com>
2022-10-08 13:12:17 -0700

    Add database migration for server compute resources

---
+++ database/migrations/2022_10_08_060531_create_server_compute_resources_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * The table associated with the migration.
+     *
+     * @var string
+     */
+    protected $table = 'server_compute_resources'; 
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists($this->table);
+    }
+};
