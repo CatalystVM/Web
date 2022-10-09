99f5f47cf2d423a32d2e9141ca5a3c288c3f0683
Carson Hopper <carson.hopper@outlook.com>
2022-10-09 10:33:54 -0700

    Sidebar item can now have sub items

---
+++ resources/vue/Pages/Stern/Server/Virtual/Node/Index.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <Head title="Nodes" />
+    Virtual Machine
+</template>
+
+<script>
+import Layout from '@/Layouts/Stern/Layout.vue'
+
+export default {
+    layout: (h, page) => {
+        return h(Layout, {
+            Search: false, topbar: true, sidebar: true
+        }, () => page)
+    }
+}
+</script>
