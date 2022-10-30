067d473a8df21493e6b68a1dde624543f26c9aba
Carson Hopper <carson.hopper@outlook.com>
2022-10-29 21:06:44 -0400

    Create login & register page

---
+++ pages/register.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const config = useRuntimeConfig();
+
+useHead({
+    title: `Register | ${config.app.name}`
+})
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
